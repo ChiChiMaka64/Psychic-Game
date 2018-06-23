d7b3fa8e58df754cf094f1121932d12a662171a3
Lynette Okoroike <lcokoroike@gmail.com>
2018-06-23 09:53:25 -0700

    made changes to the original.Updated version.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,4 +1,36 @@
 <script type ="text/javascript">
+var wins =0;
+        var loses=0;
+        var guessesLeft= 10;
+        var totalGuessesSoFar=0;
+
     
+
+    var ComputerChoices=[ "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v","w", "x", "y", "z"];
+    var ComputerLetter=ComputerChoices[Math.floor(Math.random() * ComputerChoices.length)];
+    console.log(ComputerLetter);
+    document.onkeyup = function(event) {
+       var UserGuess = event.key;
+       var textnode = document.createTextNode(UserGuess);
+       document.getElementById("totalGuessesSoFar").appendChild(textnode);
+        if (guessesLeft === 0) {
+            alert ("No More Guesses!");
+            return;
+        }
+        
+        guessesLeft--; 
+        if (UserGuess===ComputerLetter) {
+            wins++;
+        }
+        else {
+            loses++;
+        }
+        
+         
+        document.getElementById("wins").textContent="Wins: " + wins;
+        document.getElementById("loses").textContent="Losses: " + loses;
+        document.getElementById("guessesLeft").textContent="Guesses Left: " + guessesLeft;
+        
+       
         
 </script>
